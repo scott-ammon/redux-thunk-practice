bd9da950de61451a799ba747e216e34cc92c8f1e
Scott Ammon <scott.ammon@slalom.com>
2018-11-06 08:32:24 -0800

    added prop-types, refactored apidisplay

--- src/components/ApiDisplay.js
+++ src/components/ApiDisplay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const mapStateToProps = state => {
   return {
@@ -12,15 +13,29 @@ const mapStateToProps = state => {
 
 class ApiDisplay extends Component {
 
+  static propTypes = {
+    count: PropTypes.number.isRequired,
+    latestLaunch: PropTypes.object,
+    nextLaunch: PropTypes.object,
+  }
+
+  static defaultProps = {
+    count: 0,
+    latestLaunch: {},
+    nextLaunch: {},
+  }
+
   render() {
     return (
       <Style>
         <h4>API Data</h4>
-        <h4>API Call Count: {this.props.count}</h4>
+        <h4>Call Count: {this.props.count}</h4>
+        <br></br>
         <h4>Latest Launch:</h4>
         <p>Mission Name: {this.props.latestLaunch.missionName}</p>
         <p>Rocket Name: {this.props.latestLaunch.rocketName}</p>
         <p>Launch Date: {this.props.latestLaunch.launchDate}</p>
+        <br></br>
         <h4>Next Launch:</h4>
         <p>Mission Name: {this.props.nextLaunch.missionName}</p>
         <p>Rocket Name: {this.props.nextLaunch.rocketName}</p>
